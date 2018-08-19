a91eef6f0c5dcd71106d34211f0287e8bf6d9a0b
josh-kramer-dev <josh.kramer.dev@gmail.com>
2018-08-19 01:26:32 -0700

    migrated db, but deck.name attribute not showing up for some reason. troubleshooting now

--- db/migrate/20180819073924_create_cards.rb
+++ db/migrate/20180819073924_create_cards.rb
@@ -1,7 +1,10 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-
+      t.string :name
+      t.integer :quantity
+      t.integer :deck_id
+      t.integer :format_id
       t.timestamps
     end
   end

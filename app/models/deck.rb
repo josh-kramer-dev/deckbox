02262610e318b1977b9264cd22eab4453abb01af
josh-kramer-dev <josh.kramer.dev@gmail.com>
2018-08-20 17:29:10 -0700

    updated all models. will start on sessions next

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,2 +1,8 @@
 class Deck < ApplicationRecord
+    has_many :cards 
+    belongs_to :user
+    belongs_to :format 
+
+    validates :name, presence: true
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :decks
+    has_many :cards, through: :decks
+    has_many :formats, through: :decks 
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { in: 6..20 }
+
+    def self.find_or_create_by_facebook(auth_hash)
+        user = User.find_or_create_by(:uid => auth_hash['uid']) do |u|
+            u.name = auth_hash['info']['name']
+            u.email = auth_hash['info']['email']
+            u.password = SecureRandom.hex(9)
+        end
 end

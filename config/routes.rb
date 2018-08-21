8abac6550c674db2dc37e7a3e9dd229f89c21a60
josh-kramer-dev <josh.kramer.dev@gmail.com>
2018-08-21 15:54:20 -0700

    reinstalled all gems to resolve dependency issues. will start migrating file info from deckStorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
   resources :users
-  resources :decks
-  resources :cards
+  resources :users do
+    resources :decks
+    resources :formats
+  end
+  scope :decks do
+    resources :cards
+  end
+
+  post '/decks/cards/new', to: 'cards#new'
+
+  root 'welcome#home'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/session', to: 'sessions#create', as: 'session'
+  delete '/session/', to: 'sessions#destroy', as: 'logout'
+
+
+
+  get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

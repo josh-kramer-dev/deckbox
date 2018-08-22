49c39e4dd99d6ed895c268dd0374d7be57489d1d
josh-kramer-dev <josh.kramer.dev@gmail.com>
2018-08-22 10:40:15 -0700

    stuck on getting jquery to do anything. may be a turbolinks issue? '

---
+++ app/controllers/formats_controller.rb
@@ -0,0 +1,8 @@
+class FormatsController < ApplicationController
+    def show
+      @formats = Format.all
+      @format = Format.find(params[:id])
+      @user = current_user
+      @decks = @user.decks.filter_by_format(params[:id])
+    end
+end
